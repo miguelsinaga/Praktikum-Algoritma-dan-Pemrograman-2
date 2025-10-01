b6768bc54755770b0d735f05e44ccbd454fac2a4
miguelsinaga <miguelsinaga06@gmail.com>
2025-10-01 18:55:44 +0700

    fix: fixing compile error

--- praktikum3/ColumnOperations.hs
+++ praktikum3/ColumnOperations.hs
@@ -11,22 +11,19 @@ konso e l = [e] ++ l
 konsdot l e = l ++ [e]
 isEmpty l = null l
 
--- Lengkapi Fungsi di bawah ini
--- columnOperations :: [[Int]] -> [Int]
--- columnOperations a 
--- columnOperations :: [[Int]] -> [Int]
+
+transpose :: [[Int]] -> [[Int]]
+transpose [] = []
+transpose ([]:_) = []
+transpose x = map head x : transpose (map tail x)
+
 columnOperations :: [[Int]] -> [Int]
 columnOperations a
 	| null a = []
 	| null (head a) = []
 	| otherwise = zipWith applyOp [0..] (transpose a)
 	where
-		-- repeating operations: max, min, sum
+
 		ops = cycle [maximum, minimum, sum]
 		applyOp i col = (ops !! i) col
 
--- transpose for rectangular matrices (each row same length)
-transpose :: [[Int]] -> [[Int]]
-transpose [] = []
-transpose ([]:_) = []
-transpose x = map head x : transpose (map tail x)
