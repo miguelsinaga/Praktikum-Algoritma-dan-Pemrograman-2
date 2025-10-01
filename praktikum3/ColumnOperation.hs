f6679ad98fa4b20678986f12d85dd7ae1538b660
miguelsinaga <miguelsinaga06@gmail.com>
2025-10-01 18:42:09 +0700

    add: adding new solutions

--- praktikum3/ColumnOperation.hs
+++ praktikum3/ColumnOperation.hs
@@ -14,3 +14,19 @@ isEmpty l = null l
 -- Lengkapi Fungsi di bawah ini
 -- columnOperations :: [[Int]] -> [Int]
 -- columnOperations a 
+-- columnOperations :: [[Int]] -> [Int]
+columnOperations :: [[Int]] -> [Int]
+columnOperations a
+	| null a = []
+	| null (head a) = []
+	| otherwise = zipWith applyOp [0..] (transpose a)
+	where
+		-- repeating operations: max, min, sum
+		ops = cycle [maximum, minimum, sum]
+		applyOp i col = (ops !! i) col
+
+-- transpose for rectangular matrices (each row same length)
+transpose :: [[Int]] -> [[Int]]
+transpose [] = []
+transpose ([]:_) = []
+transpose x = map head x : transpose (map tail x)
